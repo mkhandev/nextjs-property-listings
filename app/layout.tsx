d1899e686a23b66aa6a9f6336a7f1a7f6e79a789
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-07-26 16:44:46 +0600

    add Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
