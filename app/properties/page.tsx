cc000e977f67d06f135215589cf10e95f6e143c9
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-07-27 21:04:39 +0600

    add property

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,5 +1,22 @@
+import PropertyCard from "@/components/PropertyCard";
+import properties from "@/properties.json";
+
 const PropertiesPage = () => {
-  return <div>PropertiesPage</div>;
+  return (
+    <div className="px-4 py-6">
+      <div className="py-8 m-auto container-xl lg:container">
+        {properties.length === 0 ? (
+          <p>No Properties Found</p>
+        ) : (
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default PropertiesPage;
