ec7f8d0865db4ff4f1d7092db719da9769638609
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-08-02 01:23:04 +0600

    add bookmark

---
+++ app/properties/saved/page.tsx
@@ -0,0 +1,40 @@
+import PropertyCard from "@/components/PropertyCard";
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+const SavedPropertiesPage = async () => {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+
+  const { userId } = sessionUser;
+
+  // NOTE: here we can make one database query by using Model.populate
+  const { bookmarks } = await User.findById(userId)
+    .populate("bookmarks")
+    .lean<{ bookmarks: any[] } | null>();
+
+  console.log(bookmarks);
+
+  return (
+    <section className="px-4 py-6">
+      <div className="px-4 py-6 m-auto container-xl lg:container">
+        <h1 className="mb-4 text-2xl">Saved Properties</h1>
+        {bookmarks.length === 0 ? (
+          <p>No saved properties</p>
+        ) : (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            {bookmarks.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+export default SavedPropertiesPage;
