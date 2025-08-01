4d658db9ae775ff5e06307c6db639790274bb4e8
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-08-02 01:26:59 +0600

    add bookmark

--- app/properties/saved/page.tsx
+++ app/properties/saved/page.tsx
@@ -14,11 +14,11 @@ const SavedPropertiesPage = async () => {
   const { userId } = sessionUser;
 
   // NOTE: here we can make one database query by using Model.populate
-  const { bookmarks } = await User.findById(userId)
+  const user = await User.findById(userId)
     .populate("bookmarks")
     .lean<{ bookmarks: any[] } | null>();
 
-  console.log(bookmarks);
+  const bookmarks = user?.bookmarks ?? [];
 
   return (
     <section className="px-4 py-6">
